3ad2f83ca0f2a3fae36b582442d6bcbf8c381bec
777Makss777 <777docmax777@gmail.com>
2026-04-09 23:39:40 +0200

    .....

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,5 +1,38 @@
-import SidebarNotes from './SidebarNotes'
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { noteTags } from '@/types/note'
+
+import css from './SidebarNotes.module.css'
+
+const sidebarLinks = [
+  {
+    href: '/notes/filter/all',
+    label: 'All notes',
+  },
+  ...noteTags.map((tag) => ({
+    href: `/notes/filter/${tag}`,
+    label: tag,
+  })),
+]
 
 export default function DefaultSidebar() {
-  return <SidebarNotes />
+  const pathname = usePathname()
+
+  return (
+    <ul className={css.menuList}>
+      {sidebarLinks.map(({ href, label }) => (
+        <li key={href} className={css.menuItem}>
+          <Link
+            href={href}
+            className={`${css.menuLink} ${pathname === href ? css.active : ''}`}
+          >
+            {label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
 }
